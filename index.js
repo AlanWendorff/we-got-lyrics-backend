e20481c637cff42991c708c0fa97105daeb7b38e
KremoWy <alanwff4@gmail.com>
2021-02-09 16:42:21 -0300

    cors config

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const cors           = require('cors');
 // initialize the app
 const app = express();
 
+res.header("Acces-Control-Allow-Origin", "*");
+app.use(function (req, res, next) {
+  res.header("Acces-Control-Allow-Origin", "*");
+  res.header("Acces-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 // set the app's functions
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
