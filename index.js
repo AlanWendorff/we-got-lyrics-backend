544288e68cdfaed73bb23d06bfa0acc9abf9edb2
KremoWy <alanwff4@gmail.com>
2021-02-09 16:28:10 -0300

    +app.get petition

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 // call the API
-
-// no extra url needed!
-axios.get('https://api.pandascore.co/csgo/matches?&token=yVPKLDCsTsxGSJcEWb_gbzDiC6NSWVQ3thriZ3Qft_p6lGvLxPc')
-.then(data => {
-  // use CircularJSON because the response
-  // has circular references in it
-  console.log(data);
-  //res.send(data);
-})
-// check for errors
-.catch(error=> {
-  console.log(error.message);
-})
-
+app.get('/', (req, res) => {
+  // no extra url needed!
+  axios.get('https://api.pandascore.co/csgo/matches?&token=yVPKLDCsTsxGSJcEWb_gbzDiC6NSWVQ3thriZ3Qft_p6lGvLxPc')
+  .then(data => {
+    // use CircularJSON because the response
+    // has circular references in it
+    console.log(data);
+    res.send(data);
+  })
+  // check for errors
+  .catch(error=> {
+    console.log(error.message);
+  })
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, "0.0.0.0", () => {
