754c9fa81afa7cdc930cce5d5f0b7980b0b87405
KremoWy <alanwff4@gmail.com>
2021-03-31 16:17:49 -0300

    parseInt(teamId)

--- routes/prevmatches.js
+++ routes/prevmatches.js
@@ -8,11 +8,13 @@ const callAPI = async (teamId) => {
         let respuestaAPI = await axios.get(`https://api.pandascore.co/csgo/matches/past?filter[opponent_id]=${teamId}&filter[finished]=true&token=yVPKLDCsTsxGSJcEWb_gbzDiC6NSWVQ3thriZ3Qft_p6lGvLxPc`)
         let {data} = respuestaAPI;
         let imageTeam;
-        if (data[0].opponents[0].opponent.id === teamId) {
+ 
+        if (data[0].opponents[0].opponent.id === parseInt(teamId)) {
             imageTeam = data[0].opponents[0].opponent.image_url;
         }else{
             imageTeam = data[1].opponents[1].opponent.image_url;
         }
+        
 
         return {
             data: data,
