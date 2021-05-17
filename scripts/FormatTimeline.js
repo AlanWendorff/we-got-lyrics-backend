46d8b43a4c3e445de3be1638e6c16531cce67646
KremoWy <alanwff4@gmail.com>
2021-05-17 20:53:30 -0300

    palette timeline

--- scripts/FormatTimeline.js
+++ scripts/FormatTimeline.js
@@ -1,23 +1,30 @@
-const formatTimeline = (apiTimeline) => {
-  let timelineFormatted = [];
+const getColor = require("../scripts/ExtractColorOther");
+
+const formatTimeline = async (apiTimeline) => {
   const timeline = apiTimeline.data.filter((date) => date.begin_at !== null);
-  timeline.map((tournament) => {
-    timelineFormatted.push({
-      begin_at: tournament.begin_at,
-      league_id: tournament.league_id,
-      league: {
-        name: tournament.league.name,
-        image_url: tournament.league.image_url,
-      },
-      serie: {
-        full_name: tournament.serie.full_name,
-        tier: tournament.serie.tier,
-      },
-      name: tournament.name,
-      teams: tournament.teams,
-      prizepool: tournament.prizepool,
-    });
-  });
+  let timelineFormatted = await Promise.all(
+    timeline.map(async (tournament) => {
+      let colors = await getColor(tournament.league.image_url);
+      return {
+        begin_at: tournament.begin_at,
+        league_id: tournament.league_id,
+        league: {
+          name: tournament.league.name,
+          image_url: tournament.league.image_url,
+        },
+        serie: {
+          full_name: tournament.serie.full_name,
+          tier: tournament.serie.tier,
+        },
+        name: tournament.name,
+        teams: tournament.teams,
+        prizepool: tournament.prizepool,
+        colors: {
+          DarkVibrant: colors.DarkVibrant,
+        },
+      };
+    })
+  );
   return timelineFormatted;
 };
 
